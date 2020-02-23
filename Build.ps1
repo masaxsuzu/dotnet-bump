b408b51debdd65e1d7435485a5dc6b1ec3e9a877
masaxsuzu <masaxsuzu+github@gmail.com>
2020-02-23 17:35:20 +0900

    Move testcases into xunit

--- Build.ps1
+++ Build.ps1
@@ -1,55 +1,5 @@
-
-function Assert{
-	param (
-		$command,
-		$in,
-		$want
-	)
-	
-	$bump = ".\Netsoft.Tools.Bump\bin\Release\netcoreapp3.0\dotnet-bump.exe"
-
-	$got = & $bump $command $in
-
-	if($got -ne $want){
-		Write-Error("
-	Got :$got
-	Want:$want")
-	}
-	else{
-		Write-Host("OK: ${command} ${in} => ${want}")
-	}
-}
-
-function Assert-Error{
-	param (
-		$command,
-		$in,
-		$want
-	)
-	
-	$bump = ".\Netsoft.Tools.Bump\bin\Release\netcoreapp3.0\dotnet-bump.exe"
-
-	$got = & $bump $command $in 2>&1
-
-	if(!("$got".Equals("$want"))){
-		Write-Error("
-	Got :$got
-	Want:$want")
-	}
-	else{
-		Write-Host("OK: ${command} ${in} => ${want}")
-	}
-}
-
 function Run {
 	dotnet test /p:configuration="Release"
-
-	Assert-Error "major" "1"	"Version string was less than 2 digits."
-	Assert-Error "major" "1.x"	"Input string was not in a correct format."
-
-	Assert "major" "1.2.3" "2.0.0.0"
-	Assert "minor" "1.2.3" "1.3.0.0"
-	Assert "patch" "1.2.3" "1.2.4.0"
 }
 
 Run
--- Tests.Netsoft.Tools.Bump/TestProgram.cs
+++ Tests.Netsoft.Tools.Bump/TestProgram.cs
@@ -31,5 +31,42 @@ namespace Tests.Netsoft.Tools.Bump
 
             Assert.Equal(want, got);
         }
+
+        [Theory]
+        [InlineData("2.0.0.0\r\n", "major", "1.2.3")]
+        [InlineData("1.3.0.0\r\n", "minor", "1.2.3")]
+        [InlineData("1.2.4.0\r\n", "patch", "1.2.3")]
+        public void TestConsoleOut(string want, params string[] args)
+        {
+            using(var mem = new System.IO.MemoryStream())
+            {
+                using(var stdout = new System.IO.StreamWriter(mem))
+                {
+                    Console.SetOut(stdout);
+
+                    _ = Program.Main(args);
+                }
+                var got = Encoding.UTF8.GetString(mem.ToArray());
+                Assert.Equal(want,got);
+            }
+        }
+
+        [Theory]
+        [InlineData("Version string was less than 2 digits.\r\n", "major", "1")]
+        [InlineData("Input string was not in a correct format.\r\n", "major", "1.2.x")]
+        public void TestConsoleError(string want, params string[] args)
+        {
+            using(var mem = new System.IO.MemoryStream())
+            {
+                using(var stderr = new System.IO.StreamWriter(mem))
+                {
+                    Console.SetError(stderr);
+
+                    _ = Program.Main(args);
+                }
+                var got = Encoding.UTF8.GetString(mem.ToArray());
+                Assert.Equal(want,got);
+            }
+        }
     }
 }
