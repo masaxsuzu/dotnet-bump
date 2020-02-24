e204c2e1f21b5064bcc4215335a79d6adca403bc
masaxsuzu <masaxsuzu+github@gmail.com>
2020-02-24 13:25:24 +0900

    Handle error on build

--- Build.ps1
+++ Build.ps1
@@ -1,5 +1,27 @@
 function Run {
-	dotnet test /p:configuration="Release"
+	
+	dotnet test /p:configuration=Release
+
+	StopIfError "Test error"
+
+	dotnet pack --output pkg /p:configuration=Release --no-build
+
+	StopIfError "Package error"
+
 }
 
+function StopIfError{
+	param(
+		$message
+	)
+
+	if($?)
+	{
+		return
+	}
+
+	Write-Error $message
+}
+
+$ErrorActionPreference = "Stop"
 Run
